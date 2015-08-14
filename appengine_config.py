ae8ca38b3365229ce38ce82089305daab78e5d25
andrei <andrei@socrative.com>
2015-08-14 19:42:18 +0300

    updated aws url

--- appengine_config.py
+++ appengine_config.py
@@ -18,17 +18,21 @@ import os
 # gcloud preview app deploy ./app.yaml --version 1 --set-default
 
 
+# AWS CONNECT
+# ssh -i main-server-key.pem ec2-user@52.10.187.83
+
+
 # if we are currently in production
 DEV = os.environ.get('SERVER_SOFTWARE', 'Development').startswith('Development')
 # use this to simulate production
-DEV = False
+# DEV = False
 
 PRODUCTION = not DEV
 
 
 # used to communicate with aws
 SECRET_KEY = "B1={kpE_4To5ZSJW=hNx(EYDj0-f|YT8uz5*SU6iA~.A+]aWSC#lmu;<Hc|T^V@-:#|+g0b<[+toRMtyqdtEDJ$%o4$>_yRTxbsh}%a|k)BS}u;dU~%Da;;SDhmZFl[_Wpr#U?A8"
-AWS_PDF_URL = "ec2-52-10-187-83.us-west-2.compute.amazonaws.com:8090" if PRODUCTION else "http://127.0.0.1:8090"
+AWS_PDF_URL = "http://ec2-52-10-187-83.us-west-2.compute.amazonaws.com:8090" if PRODUCTION else "http://127.0.0.1:8090"
 
 
 GEOIP_SERVICES = ["http://ip-api.com/json/{0}", "http://freegeoip.net/json/{0}"]
