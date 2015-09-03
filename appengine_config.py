a7fc6ac6235af60dde859918e90485333ab837c3
Andrei Onel <andrei@donezsi.eu>
2015-09-03 14:21:45 +0300

    add the robots txt file

--- appengine_config.py
+++ appengine_config.py
@@ -16,9 +16,9 @@ import os
 # 
 # gcloud config set project donezsieu-server
 # gcloud preview app deploy ./app.yaml --version 6 --set-default
-# gcloud preview app deploy ./app.yaml --version 11
+# gcloud preview app deploy ./app.yaml --version 14
 
-# minify-css && gcloud preview app deploy ./app.yaml --version 13
+# minify-css && gcloud preview app deploy ./app.yaml --version 14
 
 
 # AWS CONNECT
