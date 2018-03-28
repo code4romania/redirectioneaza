95647106b0fa29b926202efe7e0b11433cd4ce3a
Andrei <andrei@donezsi.eu>
2018-03-28 14:35:20 +0300

    Updated the Readmme file Changed how we built the routes. Use the handler directly and not by string

--- main.py
+++ main.py
@@ -7,9 +7,13 @@ from appengine_config import SESSION_SECRET_KEY, DEV
 from webapp2_extras import routes
 from webapp2 import Route as r
 
+from controllers.site import *
+from controllers.account_management import *
+from controllers.my_account import *
+from controllers.api import *
+from controllers.admin import *
 from controllers.ngo import NgoHandler, TwoPercentHandler, TwoPercent2Handler, DonationSucces
 
-from controllers.site import NotFoundPage, InternalErrorPage
 
 config = {
     'webapp2_extras.auth': {
@@ -37,43 +41,43 @@ config = {
 # use string in dotted notation to be lazily imported
 app = webapp2.WSGIApplication([
         # the public part of the app
-        r('/',                  handler="controllers.site.HomePage"),
-        r('/ong',               handler="controllers.site.ForNgoHandler"),
+        r('/',                  handler=HomePage),
+        r('/ong',               handler=ForNgoHandler),
         
         # backup in case of old urls. to be removed
-        r('/pentru-ong-uri',    handler="controllers.site.ForNgoHandler"),
+        r('/pentru-ong-uri',    handler=ForNgoHandler),
         
-        r('/asociatii',         handler="controllers.site.NgoListHandler"),
+        r('/asociatii',         handler=NgoListHandler),
 
-        r('/termeni',    handler="controllers.site.TermsHandler"),
-        r('/TERMENI',    handler="controllers.site.TermsHandler"),
-        r('/politica',   handler="controllers.site.PolicyHandler"),
+        r('/termeni',    handler=TermsHandler),
+        r('/TERMENI',    handler=TermsHandler),
+        r('/politica',   handler=PolicyHandler),
 
         # account management
-        r('/cont-nou',  handler="controllers.account_management.SignupHandler"),
-        r('/login',     handler="controllers.account_management.LoginHandler", name='login'),
-        r('/logout',    handler="controllers.account_management.LogoutHandler", name='logout'),
+        r('/cont-nou',  handler=SignupHandler),
+        r('/login',     handler=LoginHandler, name='login'),
+        r('/logout',    handler=LogoutHandler, name='logout'),
 
-        r('/forgot',    handler="controllers.account_management.ForgotPasswordHandler", name='forgot'),
+        r('/forgot',    handler=ForgotPasswordHandler, name='forgot'),
         
         # verification url: used for signup, and reset password
-        r('/<type:v|p>/<user_id:\d+>-<signup_token:.+>', handler="controllers.account_management.VerificationHandler", name='verification'),
-        r('/password',  handler="controllers.account_management.SetPasswordHandler"),
+        r('/<type:v|p>/<user_id:\d+>-<signup_token:.+>', handler=VerificationHandler, name='verification'),
+        r('/password',  handler=SetPasswordHandler),
         
         # my account
-        r('/contul-meu',        handler="controllers.my_account.MyAccountHandler", name='contul-meu'),
-        r('/asociatia',         handler="controllers.my_account.NgoDetailsHandler", name='asociatia'),
-        r('/date-cont',         handler="controllers.my_account.MyAccountDetailsHandler", name='date-contul-meu'),
+        r('/contul-meu',        handler=MyAccountHandler, name='contul-meu'),
+        r('/asociatia',         handler=NgoDetailsHandler, name='asociatia'),
+        r('/date-cont',         handler=MyAccountDetailsHandler, name='date-contul-meu'),
 
-        r('/api/ngo/check-url/<ngo_url>', handler="controllers.api.CheckNgoUrl", name='api-ngo-check-url'),
-        r('/api/ngo/upload-url',          handler="controllers.api.GetUploadUrl", name='api-ngo-upload-url'),
+        r('/api/ngo/check-url/<ngo_url>', handler=CheckNgoUrl, name='api-ngo-check-url'),
+        r('/api/ngo/upload-url',          handler=GetUploadUrl, name='api-ngo-upload-url'),
 
         # ADMIN HANDLERS
-        r('/admin',             handler="controllers.admin.AdminHandler",       name='admin'),
-        r('/admin/conturi',     handler="controllers.admin.UserAccounts",       name='admin-users'),
-        r('/admin/campanii',    handler="controllers.admin.SendCampaign",       name='admin-campanii'),
-        r('/admin/ong-nou',     handler="controllers.admin.AdminNewNgoHandler", name='admin-ong-nou'),
-        r('/admin/<ngo_url>',   handler="controllers.admin.AdminNgoHandler",    name='admin-ong'),
+        r('/admin',             handler=AdminHandler,       name='admin'),
+        r('/admin/conturi',     handler=UserAccounts,       name='admin-users'),
+        r('/admin/campanii',    handler=SendCampaign,       name='admin-campanii'),
+        r('/admin/ong-nou',     handler=AdminNewNgoHandler, name='admin-ong-nou'),
+        r('/admin/<ngo_url>',   handler=AdminNgoHandler,    name='admin-ong'),
 
 
         r('/<ngo_url>',         handler=NgoHandler, name="ngo-url"),
