e491307c096c57fb0a03b75360b692a32f235dd7
Andrei <andrei@donezsi.eu>
2018-04-17 12:22:56 +0300

    Update the google analytics code

--- static/js/analytics.js
+++ static/js/analytics.js
@@ -6,5 +6,5 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-66333966-1', 'auto');
+ga('create', 'UA-88205556-2', 'auto');
 ga('send', 'pageview');
