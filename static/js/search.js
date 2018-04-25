3cfd82dc43f0797eab34ddc6a9243fa43b092d51
Andrei <andrei@donezsi.eu>
2018-04-25 15:16:49 +0300

    RED-36 Added a basic search on the homepage

--- static/js/search.js
+++ static/js/search.js
@@ -4,4 +4,42 @@ $(function () {
      * @type {String}
      */
     var api = '/api/ngos';
+    var searcEl = $("#search-bar");
+    var ngos = [];
+
+    function setupEasyAutocomplete() {
+        var options = {
+            data: ngos,
+            getValue: "name",
+            template: {
+                type: "iconLeft",
+                fields: {
+                    iconSrc: "logo"
+                }
+            },
+            list: {
+                onClickEvent: function () {
+                    var selected = searcEl.getSelectedItemData();
+                    window.location.href = selected.url;
+                },
+                match: {
+                    enabled: true
+                }
+            }
+        };
+
+        searcEl.easyAutocomplete(options);
+    }
+
+    $.get(api).done(function (response) {
+        ngos = response;
+        setupEasyAutocomplete()
+    })
+
+    var doit;
+    window.onresize = function(){
+      clearTimeout(doit);
+      doit = setTimeout(setupEasyAutocomplete, 100);
+    };
+
 })
