967115b0a1c67d2344b38e94a0c20888d85b8102
Daniel Ursache Dogariu <contact@danniel.net>
2023-12-26 14:34:11 +0200

    Remove unused import

--- backend/redirectioneaza/urls.py
+++ backend/redirectioneaza/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin as django_admin, admin
-from django.urls import path, re_path
+from django.urls import path
 
 from donations.views.account_management import (
     ForgotPasswordHandler,
