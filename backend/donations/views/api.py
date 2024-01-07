aa7d6a486aefbf16089e1e81ee830d4f93c748f7
Daniel Ursache Dogariu <contact@danniel.net>
2024-01-07 15:04:30 +0200

    Disable csrf checks on logo uploads

--- backend/donations/views/api.py
+++ backend/donations/views/api.py
@@ -1,3 +1,6 @@
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+
 from .base import BaseHandler, AccountHandler
 
 
@@ -20,8 +23,14 @@ class GetNgoForms(AccountHandler):
         raise NotImplementedError("GetNgoForms not implemented yet")
 
 
+@method_decorator(csrf_exempt, name='dispatch')
 class GetUploadUrl(AccountHandler):
     def get(self, request, *args, **kwargs):
+        raise NotImplementedError("GetUploadUrl not implemented yet")
+
+    def post(self, request, *args, **kwargs):
+        # logo_file = request.FILES.get("files")
+        # print(logo_file)
         raise NotImplementedError("GetUploadUrl not implemented yet")
 
 
