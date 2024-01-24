a78f4b0dc8993d2f0afeb4ac032e87f57bbcd810
Daniel Ursache Dogariu <contact@danniel.net>
2024-01-24 22:05:41 +0200

    Admin NGO list details page

--- backend/donations/views/admin.py
+++ backend/donations/views/admin.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from django.conf import settings
 from django.urls import reverse
 from django.shortcuts import redirect, render
+from django.http import Http404
 from django.utils import timezone
 
 from users.models import User
@@ -95,11 +96,49 @@ class AdminNewNgoHandler(BaseHandler):
 
 
 class AdminNgoHandler(BaseHandler):
-    pass
+    template_name = "admin2/ngo.html"
+
+    def get(self, request, ngo_url, *args, **kwargs):
+        context = {}
+
+        if not request.user.is_staff:
+            return redirect(User.create_admin_login_url(reverse("admin-ong")))
+
+        try:
+            ngo = Ngo.objects.get(slug=ngo_url)
+        except Ngo.DoesNotExist:
+            return Http404
+
+        context["ngo_upload_url"] = reverse("api-ngo-upload-url")
+        context["counties"] = settings.LIST_OF_COUNTIES
+        context["ngo"] = ngo
+
+        try:
+            context["owner"] = ngo.users.get()
+        except User.DoesNotExist:
+            context["owner"] = None
+
+        context["other_emails"] = ", ".join(str(x) for x in ngo.other_emails) if ngo.other_emails else ""
+
+        # render a response
+        return render(request, self.template_name, context)
 
 
 class AdminNgosList(BaseHandler):
-    pass
+    template_name = "admin2/ngos.html"
+
+    def get(self, request, *args, **kwargs):
+        context = {}
+
+        if not request.user.is_staff:
+            return redirect(User.create_admin_login_url(reverse("admin-ong")))
+
+        context["title"] = "Admin"
+        ngos = Ngo.objects.all()
+        context["ngos"] = ngos
+
+        # render a response
+        return render(request, self.template_name, context)
 
 
 class SendCampaign(BaseHandler):
