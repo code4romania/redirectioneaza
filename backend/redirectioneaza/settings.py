24b14ecfac0786c6018fbee644d2eed3e5c90d5d
Tudor Amariei <tudor.amariei@commitglobal.org>
2024-01-27 11:27:24 +0200

    Fix locales

--- backend/redirectioneaza/settings.py
+++ backend/redirectioneaza/settings.py
@@ -291,6 +291,7 @@ USE_I18N = True
 
 USE_TZ = True
 
+LOCALE_PATHS = (os.path.join(BASE_DIR, "locale"),)
 
 # Media & Static files storage
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
