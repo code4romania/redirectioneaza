27a476bf4cd747a41815bf3ca7f7bbcc18e2a910
Tudor Amariei <tudor.amariei@commitglobal.org>
2024-02-13 14:51:08 +0200

    Update the user model to not need a migration

--- backend/users/models.py
+++ backend/users/models.py
@@ -45,7 +45,7 @@ class User(AbstractUser):
     instead of the username
     """
 
-    id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4, editable=False)
+    id = models.UUIDField(verbose_name="ID", primary_key=True, unique=True, default=uuid.uuid4, editable=False)
 
     # We ignore the "username" field because the authentication
     # will be done by email + password
