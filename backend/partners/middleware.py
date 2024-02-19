86df2f160005fce9444eba9fedf17f4e5cb2ae5c
Daniel Ursache Dogariu <contact@danniel.net>
2024-02-19 16:14:21 +0200

    Add healthcheck endpoint

--- backend/partners/middleware.py
+++ backend/partners/middleware.py
@@ -45,6 +45,10 @@ class PartnerDomainMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
+        if request.path == "/health/":
+            logger.debug("Skipping healthcheck")
+            return self.get_response(request)
+
         logger.debug("Request host %s", request.get_host())
         try:
             subdomain = PartnerDomainMiddleware.extract_subdomain(request.get_host(), settings.APEX_DOMAIN)
