9b073610197e4ac3a1f3f1ddfd5af95b074bcc6b
Tudor Amariei <tudor.amariei@commitglobal.org>
2024-02-20 19:49:56 +0200

    Update the logos import process

--- backend/importer/admin.py
+++ backend/importer/admin.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.contrib import admin
+from django.core.management import call_command
 from django.db.models import QuerySet
 from django.utils.translation import gettext_lazy as _
 from django_q.tasks import async_task
@@ -16,7 +17,7 @@ class ImportAdmin(admin.ModelAdmin):
 
     fieldsets = ((None, {"fields": ("import_type", "csv_file", "has_header")}),)
 
-    actions = ("process_import",)
+    actions = ("process_import", "transfer_logos")
 
     @admin.action(description=_("Process the selected import jobs"))
     def process_import(self, request, queryset: QuerySet[ImportJob]):
@@ -28,3 +29,15 @@ class ImportAdmin(admin.ModelAdmin):
                 process_import_task(import_obj.id)
 
         self.message_user(request, _("The import has been processed."))
+
+    @admin.action(description=_("Schedule the transfer of logos to the current storage"))
+    def transfer_logos(self, request, queryset: QuerySet[ImportJob]):
+        call_command("import_logos", "--schedule")
+
+        self.message_user(request, _("The logos have been transferred."))
+
+    @admin.action(description=_("Schedule the transfer of donor forms to the current storage"))
+    def transfer_donor_forms(self, request, queryset: QuerySet[ImportJob]):
+        call_command("import_donor_forms")
+
+        self.message_user(request, _("The donor forms have been transferred."))
--- backend/importer/management/commands/import_logos.py
+++ backend/importer/management/commands/import_logos.py
@@ -1,27 +1,31 @@
 import logging
 import mimetypes
-import requests
 import tempfile
 
-from django.db.models import Q
+import requests
 from django.core.files import File
 from django.core.management import BaseCommand
+from django.db.models import Q
+from django_q.tasks import schedule
 
 from donations.models.main import Ngo
-
 
 logger = logging.getLogger(__name__)
 
 
 def _import_logos(batch_size=50):
     """
-    Download an reupload the logo files one by one
+    Download and re-upload the logo files batch by batch
     """
-    for ngo in (
-        Ngo.objects.exclude(Q(logo_url__isnull=True) | Q(logo_url=""))
-        .filter(Q(logo="") | Q(logo__isnull=True))
-        .all()[:batch_size]
-    ):
+    target_ngos = (
+        Ngo.objects.exclude(Q(logo_url__isnull=True) | Q(logo_url="")).filter(Q(logo="") | Q(logo__isnull=True)).all()
+    )
+
+    if target_ngos.count() == 0:
+        logger.info("No logos to import")
+        return
+
+    for ngo in target_ngos[:batch_size]:
         logger.debug("Processing logo for %s", ngo.name)
 
         if not ngo.logo_url.startswith("http"):
@@ -43,6 +47,27 @@ def _import_logos(batch_size=50):
 class Command(BaseCommand):
     help = "Import NGO logos from the old storage"
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--schedule",
+            action="store_true",
+            help="Schedule the import for later",
+        )
+
     def handle(self, *args, **options):
+        should_schedule = options.get("schedule", False)
+
+        if should_schedule:
+            schedule(
+                "donations.management.commands.import_logos._import_logos",
+                500,
+                name="IMPORT_LOGOS",
+                minutes=5,
+                repeats=-1,
+            )
+            self.stdout.write(self.style.SUCCESS("NGO logo import scheduled"))
+            return
+
         _import_logos()
+
         self.stdout.write(self.style.SUCCESS("NGO logo import done"))
