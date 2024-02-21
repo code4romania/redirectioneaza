7d1754be8db8947ec7451c5be21819e06ba8d743
Daniel Ursache Dogariu <contact@danniel.net>
2024-02-21 12:18:07 +0200

    Refactor zippy2 (#182)
    * Zippy, but different
    * Add the current date to the job directory name
    * Refactor zippy2
    * Improve logging, add download retries, fix pdf filename
    * Export only donations for the current year
    
    ---------
    
    Co-authored-by: Tudor Amariei <tudor.amariei@commitglobal.org>

--- backend/donations/models/jobs.py
+++ backend/donations/models/jobs.py
@@ -6,6 +6,14 @@ from .main import Ngo
 from users.models import User
 
 
+class JobError(Exception):
+    pass
+
+
+class JobDownloadError(JobError):
+    pass
+
+
 class JobStatusChoices(models.TextChoices):
     NEW = "new", _("New")
     ERROR = "error", _("Error")
@@ -13,7 +21,7 @@ class JobStatusChoices(models.TextChoices):
 
 
 class Job(models.Model):
-    """Keep track for download jobs"""
+    """Keep track of download jobs"""
 
     ngo = models.ForeignKey(Ngo, verbose_name=_("NGO"), on_delete=models.CASCADE, db_index=True)
     owner = models.ForeignKey(User, verbose_name=_("owner"), on_delete=models.CASCADE, db_index=True)
@@ -26,8 +34,17 @@ class Job(models.Model):
         max_length=5,
         db_index=True,
     )
-    url = models.URLField(verbose_name=_("URL"), blank=True, null=False, default="", max_length=255)
+
+    zip = models.FileField(verbose_name=_("ZIP"), upload_to="donation-zips/%Y/%m/%d/", blank=True, null=True)
+
     date_created = models.DateTimeField(verbose_name=_("date created"), db_index=True, auto_now_add=timezone.now)
+    date_finished = models.DateTimeField(verbose_name=_("date finished"), db_index=True, blank=True, null=True)
 
     def __str__(self):
         return f"{self.owner} {self.ngo} {self.status}"
+
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        if self.status == JobStatusChoices.DONE and not self.date_finished:
+            self.date_finished = timezone.now()
+
+        super().save(force_insert, force_update, using, update_fields)
---
+++ backend/donations/views/donations_download.py
@@ -0,0 +1,109 @@
+import io
+import logging
+from datetime import datetime
+from zipfile import ZipFile
+
+import requests
+from django.db.models import QuerySet
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+
+from donations.models.jobs import Job, JobStatusChoices, JobDownloadError
+from donations.models.main import Donor, Ngo
+from redirectioneaza.common.messaging import send_email
+
+
+logger = logging.getLogger(__name__)
+
+
+def download_donations_job(job_id: int = 0):
+    try:
+        job: Job = Job.objects.select_related("ngo").get(id=job_id)
+    except Job.DoesNotExist:
+        logger.error("Job with ID %d does not exist", job_id)
+        return
+
+    ngo: Ngo = job.ngo
+    ts = timezone.now()
+    donations: QuerySet[Donor] = Donor.objects.filter(
+        ngo=ngo,
+        has_signed=True,
+        date_created__gte=datetime(year=ts.year, month=1, day=1, hour=0, minute=0, second=0, tzinfo=ts.tzinfo),
+    ).all()
+
+    try:
+        zip_byte_stream: io.BytesIO = _package_donations(donations, job, ngo)
+        job.zip.save(f"{ngo.name}.zip", zip_byte_stream.getvalue(), save=False)
+        job.status = JobStatusChoices.DONE
+        job.save()
+    except Exception as e:
+        logger.error("Error processing job %d: %s", job_id, e)
+        job.status = JobStatusChoices.ERROR
+        job.save()
+        return
+
+    _announce_done(job)
+
+
+def _package_donations(donations: QuerySet[Donor], job: Job, ngo: Ngo) -> io.BytesIO:
+    zip_byte_stream = io.BytesIO()
+
+    with ZipFile(zip_byte_stream, mode="w", compresslevel=1) as zip_archive:
+        logger.info("Processing %d donations for '%s'", len(donations), ngo.name)
+        for donation in donations:
+            source_url: str  # Current donation's PDF file URL
+
+            # The 'pdf_file' property has priority over the old 'pdf_url' one
+            if donation.pdf_file:
+                source_url = donation.pdf_file.url
+            elif donation.pdf_url:
+                source_url = donation.pdf_url
+            else:
+                source_url = ""
+
+            if not source_url:
+                logger.info("Donation #%d has no PDF URL", donation.id)
+                continue
+            else:
+                logger.info("Donation #%d PDF URL: '%s'", donation.id, source_url)
+
+            retries_left = 2
+            while retries_left > 0:
+                try:
+                    pdf_content: bytes = _download_file(source_url)
+                except JobDownloadError:
+                    retries_left -= 1
+                    logger.error("Could not download '%s'. Retries left %d.", source_url, retries_left)
+                except Exception as e:
+                    retries_left = 0
+                    logger.error("Could not download '%s'. Exception %s", source_url, e)
+                else:
+                    retries_left = 0
+                    donation_timestamp = donation.date_created or timezone.now()
+                    filename = datetime.strftime(donation_timestamp, "%Y%m%d_%H%M") + f"__{donation.id}.pdf"
+                    with zip_archive.open(filename, "w") as archive_file:
+                        archive_file.write(pdf_content)
+
+    return zip_byte_stream
+
+
+def _download_file(source_url: str) -> bytes:
+    if not source_url:
+        raise ValueError("source_url is empty")
+
+    response = requests.get(source_url, stream=True).content
+
+    if response.status_code != 200:
+        raise JobDownloadError
+    else:
+        return response
+
+
+def _announce_done(job: Job):
+    send_email(
+        subject=_("Documents ready for download"),
+        to_emails=[job.ngo.email],
+        text_template="email/zipped_forms/zipped_forms.txt",
+        html_template="email/zipped_forms/zipped_forms.html",
+        context={"link": job.zip.url},
+    )
