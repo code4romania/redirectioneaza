ac7106ef637826ff824c2fc764c4ee2552a851f0
Daniel Ursache Dogariu <contact@danniel.net>
2024-02-23 06:29:39 +0200

    Improve archive naming

--- backend/donations/views/donations_download.py
+++ backend/donations/views/donations_download.py
@@ -27,18 +27,21 @@ def download_donations_job(job_id: int = 0):
         return
 
     ngo: Ngo = job.ngo
-    ts = timezone.now()
+    timestamp = timezone.now()
     donations: QuerySet[Donor] = Donor.objects.filter(
         ngo=ngo,
         has_signed=True,
-        date_created__gte=datetime(year=ts.year, month=1, day=1, hour=0, minute=0, second=0, tzinfo=ts.tzinfo),
+        date_created__gte=datetime(
+            year=timestamp.year, month=1, day=1, hour=0, minute=0, second=0, tzinfo=timestamp.tzinfo
+        ),
     ).all()
+
+    file_name = datetime.strftime(timestamp, "%Y%m%d_%H%M") + f"__n{ngo.id:06d}.zip"
 
     with tempfile.TemporaryDirectory(prefix=f"rdr_zip_{job_id:06d}_") as tmp_dir_name:
         logger.info("Created temporary directory '%s'", tmp_dir_name)
         try:
             zip_path = _package_donations(tmp_dir_name, donations, ngo)
-            file_name = f"{ngo.id:04f}_{ngo.name}.zip"
             with open(zip_path, "rb") as f:
                 job.zip.save(file_name, File(f), save=False)
             job.status = JobStatusChoices.DONE
@@ -73,13 +76,13 @@ def _package_donations(tmp_dir_name: str, donations: QuerySet[Donor], ngo: Ngo):
     logger.info("Processing %d donations for '%s'", len(donations), ngo.name)
 
     zip_timestamp = timezone.now()
-    zip_name = datetime.strftime(zip_timestamp, "%Y%m%d_%H%M") + f"__{ngo.id}.zip"
+    zip_name = datetime.strftime(zip_timestamp, "%Y%m%d_%H%M") + f"__n{ngo.id:06d}.zip"
     zip_path = Path(tmp_dir_name) / zip_name
 
     zip_64_flag = len(donations) > 4000
 
     zipped_files: int = 0
-    with ZipFile(zip_path, mode="w", compression=ZIP_DEFLATED, compresslevel=9) as zip_archive:
+    with ZipFile(zip_path, mode="w", compression=ZIP_DEFLATED, compresslevel=1) as zip_archive:
         for donation in donations:
             source_url = _get_pdf_url(donation)
 
@@ -87,7 +90,7 @@ def _package_donations(tmp_dir_name: str, donations: QuerySet[Donor], ngo: Ngo):
                 continue
 
             donation_timestamp = donation.date_created or timezone.now()
-            filename = datetime.strftime(donation_timestamp, "%Y%m%d_%H%M") + f"__{donation.id}.pdf"
+            filename = datetime.strftime(donation_timestamp, "%Y%m%d_%H%M") + f"__d{donation.id:06d}.pdf"
 
             retries_left = 2
             while retries_left > 0:
@@ -116,9 +119,6 @@ def _download_file(source_url: str):
         media_root = "/".join(settings.MEDIA_ROOT.split("/")[:-1])
         with open(media_root + source_url, "rb") as f:
             return f.read()
-        #     with open(destination_path, "wb") as w:
-        #         w.write(f.read())
-        # return
 
     if not source_url:
         raise ValueError("source_url is empty")
@@ -129,8 +129,6 @@ def _download_file(source_url: str):
         raise JobDownloadError
 
     return response.content
-    # with open(destination_path, "wb") as w:
-    #     w.write(response.content)
 
 
 def _announce_done(job: Job):
