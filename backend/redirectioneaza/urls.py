b30e350cfb129edcfb51bd406aa13ff5d776b7f2
Tudor Amariei <tudor.amariei@commitglobal.org>
2024-02-25 11:40:07 +0200

    Remove campaigns from the admin

--- backend/redirectioneaza/urls.py
+++ backend/redirectioneaza/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib import admin as django_admin, admin
+from django.contrib import admin, admin as django_admin
 from django.urls import path, re_path
 from django.views.generic import RedirectView
 
@@ -34,29 +34,27 @@ from donations.views.admin import (
     AdminNewNgoHandler,
     AdminNgoHandler,
     AdminNgosList,
-    SendCampaign,
     UserAccounts,
 )
 from donations.views.api import CheckNgoUrl, GetNgoForm, GetNgoForms, GetUploadUrl, NgosApi
 from donations.views.cron import CustomExport, NgoExport, NgoRemoveForms, Stats
 from donations.views.my_account import (
+    ArchiveDownloadLinkHandler,
     MyAccountDetailsHandler,
     MyAccountHandler,
     NgoDetailsHandler,
-    ArchiveDownloadLinkHandler,
 )
-from donations.views.ngo import DonationSucces, FormSignature, TwoPercentHandler, OwnFormDownloadLinkHandler
+from donations.views.ngo import DonationSucces, FormSignature, OwnFormDownloadLinkHandler, TwoPercentHandler
 from donations.views.site import (
     AboutHandler,
     ForNgoHandler,
+    HealthCheckHandler,
     HomePage,
     NgoListHandler,
     NoteHandler,
     PolicyHandler,
     TermsHandler,
-    HealthCheckHandler,
 )
-
 
 admin.site.site_header = f"Admin | {settings.VERSION_SUFFIX}"
 
@@ -113,7 +111,6 @@ urlpatterns = (
         # Django Admin
         path("admin/django/", django_admin.site.urls),
         # ADMIN HANDLERS
-        path("admin/campanii/", SendCampaign.as_view(), name="admin-campanii"),
         path("admin/ong-nou/", AdminNewNgoHandler.as_view(), name="admin-ong-nou"),
         path("admin/conturi/", UserAccounts.as_view(), name="admin-users"),
         path("admin/organizatii/", AdminNgosList.as_view(), name="admin-ngos"),
