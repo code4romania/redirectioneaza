6e1d92983dcbb0a6ab64c413f464da8ad46d8862
Tudor Amariei <tudor.amariei@commitglobal.org>
2024-02-28 15:56:16 +0200

    Update partners admin

--- backend/partners/admin.py
+++ backend/partners/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
 from .models import Partner
+
+
+class NgoPartnerInline(admin.TabularInline):
+    model = Partner.ngos.through
+    extra = 1
+
+    verbose_name = _("NGO")
+    verbose_name_plural = _("NGOs")
 
 
 @admin.register(Partner)
@@ -11,3 +20,37 @@ class PartnerAdmin(admin.ModelAdmin):
         "is_active",
     )
     list_filter = ("is_active", "date_updated")
+
+    inlines = (NgoPartnerInline,)
+    readonly_fields = ("date_created", "date_updated")
+
+    fieldsets = (
+        (
+            _("Partner"),
+            {
+                "fields": (
+                    "subdomain",
+                    "name",
+                )
+            },
+        ),
+        (
+            _("Options"),
+            {
+                "fields": (
+                    "is_active",
+                    "has_custom_header",
+                    "has_custom_note",
+                )
+            },
+        ),
+        (
+            _("Metadata"),
+            {
+                "fields": (
+                    "date_created",
+                    "date_updated",
+                )
+            },
+        ),
+    )
