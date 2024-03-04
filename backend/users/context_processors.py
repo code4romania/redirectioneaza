757be6126277e51f018d4ce09c8e5ca1da98dd90
Tudor Amariei <tudor.amariei@commitglobal.org>
2024-03-04 13:10:26 +0200

    Revert "Use a header picker to choose which header to show a user"
    This reverts commit 93f40f0c312954ed008337056c897f970e1ed5ea.

--- backend/users/context_processors.py
+++ backend/users/context_processors.py
@@ -3,7 +3,6 @@ from django.http import HttpRequest
 
 def is_admin(request: HttpRequest):
     return {
-        "is_authenticated": request.user.is_authenticated,
         "is_admin": request.user.has_perm("users.can_view_old_dashboard"),
         "is_staff": request.user.is_staff,
     }
