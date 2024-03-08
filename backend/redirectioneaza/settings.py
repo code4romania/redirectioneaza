d6db8a94e7ac97b041f587992aaf84117fc38992
Tudor <tudor.amariei@commitglobal.org>
2024-03-08 09:49:05 +0200

    Add initial testing configuration (#230)
    - add requirements file for tests
    - add command in Makefile to configure test requirements
    - configure testing with pytest in the pyproject.toml
    - configure settings to use a .env.local file if it exists
    - move existing tests to pytest
    - write tests to check existing validations
    - add a GH Action to check the tests pass
    - remove zipper

--- backend/redirectioneaza/settings.py
+++ backend/redirectioneaza/settings.py
@@ -32,6 +32,8 @@ TEBIBYTE = GIBIBYTE * 1024
 ROOT = Path(__file__).resolve().parent.parent.parent
 BASE_DIR = os.path.abspath(os.path.join(ROOT, "backend"))
 
+ENV_FILE_NAME = os.environ.get("ENV_FILE_NAME", ".env.local")
+ENV_FILE_PATH = os.path.join(BASE_DIR, os.pardir, ENV_FILE_NAME)
 
 env = environ.Env(
     # set casting, default value
@@ -121,6 +123,7 @@ env = environ.Env(
     GOOGLE_ANALYTICS_ID=(str, ""),
 )
 
+environ.Env.read_env(ENV_FILE_PATH)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
