4c08943b83bd5a266395ff6fb1e480baaee933cd
Tudor Amariei <tudor.amariei@commitglobal.org>
2024-03-22 09:59:48 +0200

    Create a contact & faq page

---
+++ backend/static_extras/js/accordion.js
@@ -0,0 +1,18 @@
+let acc = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    let panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
