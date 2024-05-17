19c0562719bf110db651a404a13332a2f6edbf8c
Andrei Ioniță <hi@andrei.io>
2024-05-17 13:04:11 +0300

    scale up containers

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,7 @@ module "ecs_redirectioneaza" {
 
   name         = local.namespace
   cluster_name = module.ecs_cluster.cluster_name
-  min_capacity = 2
+  min_capacity = 4
   max_capacity = 8
 
   image_repo = local.image_repo
@@ -22,8 +22,8 @@ module "ecs_redirectioneaza" {
   lb_health_check_enabled = true
   lb_path                 = "/health/"
 
-  container_memory_soft_limit = 768
-  container_memory_hard_limit = 1024
+  container_memory_soft_limit = 2048
+  container_memory_hard_limit = 3072
 
   log_group_name                 = module.ecs_cluster.log_group_name
   service_discovery_namespace_id = module.ecs_cluster.service_discovery_namespace_id
