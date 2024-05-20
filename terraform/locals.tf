5a00798dac1bccd0cd25b923bdf9b03379c56a6f
Tudor Amariei <tudor.amariei@commitglobal.org>
2024-05-20 17:11:04 +0300

    infra: bump version to 2.1.4
    - customize donations limit day

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,7 +1,7 @@
 locals {
   namespace  = "redirectioneaza-${var.env}"
   image_repo = data.aws_ecr_repository.this.repository_url
-  image_tag = "2.1.3"
+  image_tag = "2.1.4"
 
   availability_zone = data.aws_availability_zones.current.names[0]
 
