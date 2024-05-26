51cbfde3bf97c5cbbd80a9db71306c40fdc1d3bb
Tudor <tudor.amariei@commitglobal.org>
2024-05-26 13:11:33 +0300

    infra: bump version to 2.1.15 (#297)

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,7 +1,7 @@
 locals {
   namespace  = "redirectioneaza-${var.env}"
   image_repo = data.aws_ecr_repository.this.repository_url
-  image_tag = "2.1.14"
+  image_tag = "2.1.15"
 
   availability_zone = data.aws_availability_zones.current.names[0]
 
