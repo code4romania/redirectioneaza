d1cb9e4ec65f1d27c105ace9cc3746aecca4a8ff
Tudor Amariei <tudor.amariei@commitglobal.org>
2024-12-05 15:22:17 +0200

    Fix linting

--- backend/donations/views/api.py
+++ backend/donations/views/api.py
@@ -11,7 +11,6 @@ from django.shortcuts import redirect
 from django.urls import reverse, reverse_lazy
 from django.utils import timezone
 from django.utils.decorators import method_decorator
-from django.utils.translation import gettext as _
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import TemplateView
 
