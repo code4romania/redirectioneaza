ac4938541187754b3d9ced415e131522d3f04f1d
Daniel Ursache Dogariu <contact@danniel.net>
2024-12-17 13:40:11 +0200

    Add a vite config

---
+++ frontend/vite.config.js
@@ -0,0 +1,44 @@
+import { defineConfig, loadEnv } from 'vite';
+import { resolve } from 'path';
+
+
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [],
+    root: resolve(__dirname, './src'),
+    base: command === 'serve' ? '/static/' : '',
+    server: {
+      open: false,
+      host: '0.0.0.0',
+      port: env.DJANGO_VITE_DEV_SERVER_PORT || 3000,
+      watch: command === 'serve' ? { usePolling: true, disableGlobbing: false } : null,
+    },
+    resolve: {
+    //   alias: {
+    //     '@': resolve(__dirname, './src'),
+    //     '@components': resolve(__dirname, './src/components'),
+    //     '@constants': resolve(__dirname, './src/constants'),
+    //     '@hooks': resolve(__dirname, './src/hooks'),
+    //   },
+      extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+    },
+    build: {
+      manifest: true,
+      emptyOutDir: true,
+      assetsDir: '',
+      target: 'es2015',
+      modulePreload: false,
+      outDir: resolve(__dirname, './dist'),
+      rollupOptions: {
+        // input: {
+        //   main: resolve('./src/main.jsx'),
+        // },
+        // output: {
+        //   chunkFileNames: undefined,
+        // },
+      },
+    },
+  };
+});
