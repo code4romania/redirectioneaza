9f697cdeaceb4972ff84ec7130111b90c27b899f
Andrei Ioniță <hi@andrei.io>
2024-12-17 16:48:06 +0200

    wip

--- backend/tailwind.config.js
+++ backend/tailwind.config.js
@@ -1,7 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    './templates/**/*.html',
+  ],
   theme: {
+    container: {
+      center: true,
+      padding: {
+          DEFAULT: '1rem',
+          sm: '1.5rem',
+          lg: '2rem',
+      },
+    },
+    fontFamily: {
+      sans: [
+        'Inter, sans-serif',
+      ],
+    },
     extend: {},
   },
   plugins: [],
