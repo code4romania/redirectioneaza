0f4c2a3883b0a411aa01767367cb5be1c16bae1f
Tudor Amariei <tudor.amariei@commitglobal.org>
2024-12-19 11:30:14 +0200

    Create a model for the fAQ

--- backend/donations/views/site.py
+++ backend/donations/views/site.py
@@ -5,11 +5,13 @@ from django.conf import settings
 from django.db.models import QuerySet
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.urls import reverse
 from django.utils import timezone
 from django.utils.safestring import mark_safe
 from django.utils.translation import gettext_lazy as _
 from django.views.generic import TemplateView
 
+from frequent_questions.models import Question
 from partners.models import DisplayOrderingChoices
 from redirectioneaza.common.cache import cache_decorator
 
@@ -112,6 +114,11 @@ class FAQHandler(TemplateView):
     template_name = "public/faq.html"
 
     def get_questions(self):
+        # TODO: transform this into an import for the FAQ model
+
+        donation_deadline = settings.DONATIONS_LIMIT
+        contact_email = settings.CONTACT_EMAIL_ADDRESS
+
         return [
             {
                 "title": _("For NGOs"),
@@ -219,7 +226,7 @@ class FAQHandler(TemplateView):
                         "title": _("Cum se poate schimba adresa de e-mail asociată contului organizației?"),
                         "answer": mark_safe(
                             _(
-                                """
+                                f"""
             <p>
                 Pentru a schimba adresa de e-mail asociată contului sunt necesari câțiva pași:
             </p>
@@ -230,7 +237,7 @@ class FAQHandler(TemplateView):
                 </li>
                 <li>
                     Trimiteți-ne un e-mail pe adresa
-                    <a href="mailto:{{ contact_email }}">{{ contact_email }}</a>
+                    <a href="mailto:{contact_email}">{contact_email}</a>
                     în care să soliciți schimbarea adresei atașate contului,
                     atașând mesajului certificatul de înregistrare al organizației
                     și un document care să ateste poziția solicitantului în ONG
@@ -285,7 +292,7 @@ class FAQHandler(TemplateView):
                     Un dosar/folder cu formularele completate de către contribuabili
                 </li>
                 <li>
-                    Un dosar/folder cu fișierele XML ce pot fi încarcă în PDF-ul ANAF
+                    Un dosar/folder cu fișierele XML ce pot fi încărcate în PDF-ul ANAF
                 </li>
             </ul>
                         """
@@ -308,29 +315,29 @@ class FAQHandler(TemplateView):
             </p>
             <ul>
                 <li>
-                Venituri din salarii și asimilate salariilor;
+                    Venituri din salarii și asimilate salariilor;
                 </li>
                 <li>
-                Venituri din pensii;
+                    Venituri din pensii;
                 </li>
                 <li>
-                Venituri din activități independente impuse pe bază de normă de venit;
+                    Venituri din activități independente impuse pe bază de normă de venit;
                 </li>
                 <li>
-                Activități agricole impuse pe bază de normă de venit;
+                    Activități agricole impuse pe bază de normă de venit;
                 </li>
                 <li>
-                Venituri din activități independente realizate în baza contractelor de
-                activitate sportivă, pentru care impozitul se reține la sursă;
+                    Venituri din activități independente realizate în baza contractelor de
+                    activitate sportivă, pentru care impozitul se reține la sursă;
                 </li>
                 <li>
-                Venituri din drepturi de proprietate intelectuală,
-                altele decât cele pentru care venitul net se determină în sistem real;
+                    Venituri din drepturi de proprietate intelectuală,
+                    altele decât cele pentru care venitul net se determină în sistem real;
                 </li>
                 <li>
-                Venituri din cedarea folosinței bunurilor pentru care venitul net se determină
-                pe baza normelor de venit și venituri din cedarea folosinței bunurilor
-                pentru care venitul net se determină pe baza cotelor forfetare de cheltuieli.
+                    Venituri din cedarea folosinței bunurilor pentru care venitul net se determină
+                    pe baza normelor de venit și venituri din cedarea folosinței bunurilor
+                    pentru care venitul net se determină pe baza cotelor forfetare de cheltuieli.
                 </li>
             </ul>
                         """
@@ -341,10 +348,10 @@ class FAQHandler(TemplateView):
                         "title": _("Cum pot redirecționa 3,5% din impozitul pe venit?"),
                         "answer": mark_safe(
                             _(
-                                """
+                                f"""
             <p>
                 Redirecționarea procentului din impozitul pe venit se face prin depunerea declarației 230
-                până la data de {{ limit.strftime("%d.%m.%Y") }}.
+                până la data de {donation_deadline.strftime("%d.%m.%Y")}.
                 Există trei variante de a depune declarația:
             </p>
             <ol>
@@ -360,7 +367,7 @@ class FAQHandler(TemplateView):
                     și ei se vor ocupa de depunerea acestora către ANAF.
                     Prin intermediul platformei Redirecționează,
                     poți face acest lucru semnând declarația
-                    <a href="{{ reverse('organizations') }}">aici</a>.
+                    <a href="{reverse('organizations')}">aici</a>.
                 </li>
             </ol>
                         """
@@ -422,11 +429,11 @@ class FAQHandler(TemplateView):
         ]
 
     def get(self, request, *args, **kwargs):
+        questions = Question.get_all()
+
         context = {
             "title": _("Frequently Asked Questions"),
-            "contact_email": settings.CONTACT_EMAIL_ADDRESS,
-            "limit": settings.DONATIONS_LIMIT,
-            "questions": self.get_questions(),
+            "questions": questions,
         }
         return render(request, self.template_name, context)
 
