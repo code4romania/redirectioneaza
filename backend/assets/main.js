17040a0ad8fabb33732163a3b13651ae13c5dc82
Andrei Ioniță <hi@andrei.io>
2024-12-20 10:29:07 +0000

    wip login and register

--- backend/assets/main.js
+++ backend/assets/main.js
@@ -1,7 +1,11 @@
 import "@fontsource/inter/latin-ext-400.css";
 import "@fontsource/inter/latin-ext-400-italic.css";
+import "@fontsource/inter/latin-ext-500.css";
+import "@fontsource/inter/latin-ext-500-italic.css";
 import "@fontsource/inter/latin-ext-600.css";
 import "@fontsource/inter/latin-ext-600-italic.css";
+import "@fontsource/inter/latin-ext-800.css";
+import "@fontsource/inter/latin-ext-800-italic.css";
 import './main.css';
 
 import Alpine from 'alpinejs';
