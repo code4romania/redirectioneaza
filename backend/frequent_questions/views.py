db944107a23b91332bea5f9951d20524c2060d8f
Tudor Amariei <tudor.amariei@commitglobal.org>
2024-12-20 11:47:46 +0200

    update faq
    - fix template
    - add proper seed data

--- backend/frequent_questions/views.py
+++ backend/frequent_questions/views.py
@@ -1,18 +1,21 @@
+from django.conf import settings
 from django.shortcuts import render
 from django.utils.translation import gettext as _
-from django.views.generic import TemplateView
 
+from donations.views.base import BaseVisibleTemplateView
 from frequent_questions.models import Question
 
 
-class FAQHandler(TemplateView):
+class FAQHandler(BaseVisibleTemplateView):
     template_name = "public/faq.html"
+    title = _("Frequently Asked Questions")
 
     def get(self, request, *args, **kwargs):
         questions = Question.get_all()
 
         context = {
             "title": _("Frequently Asked Questions"),
+            "contact_email": settings.CONTACT_EMAIL_ADDRESS,
             "questions": questions,
         }
         return render(request, self.template_name, context)
