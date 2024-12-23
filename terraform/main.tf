aa395ef9691f24e6440f9d043ed784658f1d85dd
Tudor <tudor.amariei@commitglobal.org>
2024-12-23 17:56:58 +0200

    infra: upscale resources (#351)
    - upgrade ECS to min2/max8 instances
    - upgrade DB to t4g.medium

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,8 +7,8 @@ module "ecs_redirectioneaza" {
 
   name         = local.namespace
   cluster_name = module.ecs_cluster.cluster_name
-  min_capacity = 1
-  max_capacity = 2
+  min_capacity = 2
+  max_capacity = 8
 
   image_repo = local.image_repo
   image_tag  = local.image_tag
