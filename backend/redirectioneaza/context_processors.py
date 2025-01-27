e50d84b75a71e7f85be408e2a3ffbfa94f959812
Tudor Amariei <tudor.amariei@commitglobal.org>
2025-01-27 17:15:44 +0200

    resize the dropdown

--- backend/redirectioneaza/context_processors.py
+++ backend/redirectioneaza/context_processors.py
@@ -161,6 +161,7 @@ def headers(request: HttpRequest) -> Dict[str, Dict[str, List[Dict[str, str]]]]:
                 {
                     "title": _("My Organization"),
                     "style": "auth-highlight",
+                    "content_size": "small",
                     "content": [
                         # {
                         #     "title": _("Dashboard"),
@@ -184,6 +185,9 @@ def headers(request: HttpRequest) -> Dict[str, Dict[str, List[Dict[str, str]]]]:
                             "target": "org-archives",
                             "url": reverse("my-organization:archives"),
                             "icon": "M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3",
+                        },
+                        {
+                            "separator": True,
                         },
                         {
                             "title": _("Log out"),
