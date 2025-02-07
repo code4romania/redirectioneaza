45023d6cb3ef87a6ccd99ada406a58341229ce49
Tudor Amariei <tudor.amariei@commitglobal.org>
2025-02-07 14:48:05 +0200

    improve localflavor error messages

--- backend/redirectioneaza/settings/i18n.py
+++ backend/redirectioneaza/settings/i18n.py
@@ -12,4 +12,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-LOCALE_PATHS = (os.path.join(BASE_DIR, "locale"),)
+LOCALE_PATHS = [
+    os.path.join(BASE_DIR, "locale"),
+    os.path.join(BASE_DIR, "locale_localflavor"),
+]
