6c0182e1617fd217cacd2bcf0313b05ae55db494
Tudor <tudor.amariei@commitglobal.org>
2025-02-18 16:47:52 +0200

    infra: bump to 3.0.12 (#393)

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,7 +1,7 @@
 locals {
   namespace  = "redirectioneaza-${var.env}"
   image_repo = "code4romania/redirectioneaza"
-  image_tag  = "3.0.11"
+  image_tag  = "3.0.12"
 
   availability_zone = data.aws_availability_zones.current.names[0]
 
