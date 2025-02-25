62cc21f27e7b16ead9679a75b8e0e8d3b7b412e3
Tudor Amariei <tudor.amariei@commitglobal.org>
2025-02-25 12:04:09 +0100

    Update the cause creation migration

--- backend/donations/migrations/0023_auto_20250218_1306.py
+++ backend/donations/migrations/0023_auto_20250218_1306.py
@@ -10,8 +10,12 @@ def copy_ngos_to_causes(apps, _):
     Cause = apps.get_model("donations", "Cause")
     Ngo = apps.get_model("donations", "Ngo")
 
+    batch_size = 1000
+    pending_causes = []
     for ngo in Ngo.objects.all():
         if ngo.causes.exists():
+            logger.warning("NGO %s already has causes. Skipping.", ngo)
+
             continue
 
         cause = Cause(
@@ -24,10 +28,19 @@ def copy_ngos_to_causes(apps, _):
             bank_account=ngo.bank_account,
         )
 
-        try:
-            cause.save()
-        except IntegrityError as e:
-            logger.warning("Could not save cause for NGO %s. ERROR: %s", ngo, e)
+        pending_causes.append(cause)
+
+        if len(pending_causes) >= batch_size:
+            try:
+                Cause.objects.bulk_create(pending_causes)
+                pending_causes = []
+            except IntegrityError as e:
+                logger.error("Failed to bulk create causes: %s", e)
+
+    try:
+        Cause.objects.bulk_create(pending_causes, batch_size=1000)
+    except IntegrityError as e:
+        logger.error("Failed to bulk create causes: %s", e)
 
 
 def copy_causes_to_ngos(apps, _):
@@ -51,22 +64,40 @@ def add_cause_to_donors(apps, _):
     Cause = apps.get_model("donations", "Cause")
     Donor = apps.get_model("donations", "Donor")
 
-    for donor in Donor.objects.all():
+    batch_size = 1000
+    pending_donors = []
+    for donor in Donor.objects.filter(ngo__isnull=False, cause__isnull=True):
         cause: Cause = donor.ngo.causes.first()
 
         donor.cause = cause
-        donor.save()
+        pending_donors.append(donor)
+
+        if len(pending_donors) >= batch_size:
+            Donor.objects.bulk_update(pending_donors, ["cause"])
+            pending_donors = []
+
+    if len(pending_donors) > 0:
+        Donor.objects.bulk_update(pending_donors, ["cause"])
 
 
 def add_cause_to_jobs(apps, _):
     Cause = apps.get_model("donations", "Cause")
     Job = apps.get_model("donations", "Job")
 
-    for job in Job.objects.all():
+    batch_size = 1000
+    pending_jobs = []
+    for job in Job.objects.filter(ngo__isnull=False, cause__isnull=True):
         cause: Cause = job.ngo.causes.first()
 
         job.cause = cause
-        job.save()
+        pending_jobs.append(job)
+
+        if len(pending_jobs) >= batch_size:
+            Job.objects.bulk_update(pending_jobs, ["cause"])
+            pending_jobs = []
+
+    if len(pending_jobs) > 0:
+        Job.objects.bulk_update(pending_jobs, ["cause"])
 
 
 class Migration(migrations.Migration):
