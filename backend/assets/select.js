b72c3171a7c62c1029b610a758fb1e1878a6ab7c
Ion Dormenco <idormenco@gmail.com>
2025-03-20 13:05:34 +0200

    Fix filters

---
+++ backend/assets/select.js
@@ -0,0 +1,22 @@
+export default function (options, selectedValue) {
+
+  return {
+      selectedOption: "",
+      options: [],
+
+      handleChange() {
+          this.$refs.select.closest('form').submit();
+      },
+
+      init() {
+          // Convert all option values to strings to ensure consistency
+          this.options = options.map(option => ({
+              ...option,
+              value: String(option.value) // Ensure option values are strings
+          }));
+
+          // Convert selected value to string if not null
+          this.selectedOption  = selectedValue !== null ? String(selectedValue) : "";
+      }
+  }
+}
