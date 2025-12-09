c84ebf8a0491221ab8b7abd7ec139aacbede7c2a
Tudor Amariei <tudor.amariei@commitglobal.org>
2025-12-09 13:01:36 +0200

    refactor: simplify auditlog cleanup scheduling logic

--- backend/utils/management/commands/schedule_auditlog_cleanup.py
+++ backend/utils/management/commands/schedule_auditlog_cleanup.py
@@ -1,43 +1,22 @@
 import logging
 from datetime import timedelta
 
-from django.core.management import BaseCommand
-from django.db.models import QuerySet
 from django.utils import timezone
 from django_q.models import Schedule
-from django_q.tasks import schedule
+
+from utils.common.commands import SchedulerCommand
 
 logger = logging.getLogger(__name__)
 
 
-class Command(BaseCommand):
+class Command(SchedulerCommand):
     help = "Schedule an expired auditlog cleanup task to run once a day"
+
+    command_name = "cleanup_auditlog"
+
     schedule_name = "CLEANUP_AUDITLOG"
-
-    def handle(self, *args, **kwargs):
-        logger.info("Scheduling auditlog cleanup")
-
-        self.remove_existing_schedules()
-        self.schedule_auditlog_cleanup()
-
-        logger.info("Auditlog cleanup scheduled successfully")
-
-    def remove_existing_schedules(self):
-        existing_schedules: QuerySet[Schedule] = Schedule.objects.filter(name=self.schedule_name)
-
-        if not existing_schedules.exists():
-            return
-
-        logger.info(f"Removing {existing_schedules.count()} existing schedules")
-
-        existing_schedules.delete()
-
-    def schedule_auditlog_cleanup(self):
-        schedule(
-            "django.core.management.call_command",
-            "cleanup_auditlog",
-            name=self.schedule_name,
-            schedule_type=Schedule.DAILY,
-            repeats=-1,
-            next_run=timezone.now() + timedelta(minutes=7),
-        )
+    schedule_details = {
+        "schedule_type": Schedule.DAILY,
+        "repeats": -1,
+        "next_run": timezone.now() + timedelta(minutes=7),
+    }
