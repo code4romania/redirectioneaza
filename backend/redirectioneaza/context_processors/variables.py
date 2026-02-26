4a42b90e75d9d67612a84eb8c73608ca768103cf
Daniel Ursache Dogariu <daniel.ursache@commitglobal.org>
2026-02-26 14:27:52 +0200

    Set a two column layout for authentication error pages

--- backend/redirectioneaza/context_processors/variables.py
+++ backend/redirectioneaza/context_processors/variables.py
@@ -4,5 +4,6 @@ from django.http import HttpRequest
 
 def main(_: HttpRequest) -> dict[str, dict[str, list[dict[str, str]]]]:
     return {
+        "CONTACT_EMAIL_ADDRESS": settings.CONTACT_EMAIL_ADDRESS,
         "GOOGLE_ANALYTICS_ID": settings.GOOGLE_ANALYTICS_ID,
     }
