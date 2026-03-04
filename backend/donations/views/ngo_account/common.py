c83ebffa2450c3f80746eea664bd89d474c9fb77
Daniel Ursache Dogariu <daniel.ursache@commitglobal.org>
2026-03-04 14:49:38 +0200

    Remove unused imports

--- backend/donations/views/ngo_account/common.py
+++ backend/donations/views/ngo_account/common.py
@@ -1,4 +1,3 @@
-from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import ObjectDoesNotExist, ValidationError
 from django.db.models import QuerySet
