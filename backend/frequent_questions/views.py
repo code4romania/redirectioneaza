59ebf4acf6364534bc60eee7b5d8ee9826b840b0
Daniel Ursache Dogariu <daniel.ursache@commitglobal.org>
2026-03-04 14:49:38 +0200

    Remove unused imports

--- backend/frequent_questions/views.py
+++ backend/frequent_questions/views.py
@@ -1,4 +1,3 @@
-from django.conf import settings
 from django.shortcuts import render
 from django.utils.translation import gettext as _
 
--- backend/partners/admin.py
+++ backend/partners/admin.py
@@ -1,5 +1,4 @@
 from django import forms
-from django.conf import settings
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group
