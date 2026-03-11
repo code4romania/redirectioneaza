cdc3654fa73856b01beb6e52c90a14480c35783a
Daniel Ursache Dogariu <daniel.ursache@commitglobal.org>
2026-03-11 08:12:19 +0200

    Add more logging

--- backend/utils/staging.py
+++ backend/utils/staging.py
@@ -1,3 +1,6 @@
+import logging
+
+from django.conf import settings
 from django.contrib import messages
 from django.core.exceptions import PermissionDenied
 from django.shortcuts import redirect
@@ -6,9 +9,16 @@ from django.utils.translation import gettext_lazy as _
 
 from django_q.tasks import async_task
 
+logger = logging.getLogger(__name__)
+
 
 def reset_staging(generate_orgs_count=0, generate_causes_count=0, generate_donations_count=0):
-    print("RESET TODO")
+    if settings.ENVIRONMENT not in ("staging", "development"):
+        logger.warning(
+            "Cannot reset the staging environment because the current environment is %s", settings.ENVIRONMENT
+        )
+        return
+
     pass
 
 
@@ -16,6 +26,7 @@ def schedule_reset_staging(request):
     if not request.user or not request.user.has_perm("can_reset_staging"):
         raise PermissionDenied
 
+    logger.info("Scheduling a staging environment reset")
     async_task(reset_staging, generate_orgs_count=100, generate_causes_count=100, generate_donations_count=1000)
 
     messages.add_message(
