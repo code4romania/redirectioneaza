c43f2deddef510aa6028450f178ef0f04cbbc8d1
Daniel Ursache Dogariu <daniel.ursache@commitglobal.org>
2026-03-31 11:38:15 +0300

    Simplify the query for selecting NGOs for registry checks

--- backend/donations/management/commands/check_ngos.py
+++ backend/donations/management/commands/check_ngos.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from django.core.management import BaseCommand
 from django.db.models import Q
-from django.utils import timezone
 
 from donations.models.ngos import Ngo
 from donations.workers.check_organization import cult_registry_check_organizations
@@ -11,26 +10,20 @@ class Command(BaseCommand):
     help = "Check NGOs in ANAF Cult Registry"
 
     def handle(self, *args, **options):
-        long_deadline = timezone.now() - timezone.timedelta(days=90)
-        recent_deadline = timezone.now() - timezone.timedelta(hours=1)
-        short_deadline = timezone.now() - timezone.timedelta(days=30)
-
-        registration_numbers = set(
+        qs: list[str] = (
             Ngo.objects.exclude(pause_cult_registry_check=True)
+            # check only NGOs whose registration number is valid:
             .filter(registration_number_valid=True)
-            .filter(
-                # ASAP check NGOs which are were not yet checked in the registry:
-                Q(Q(is_in_cult_registry__isnull=True) & Q(cult_registry_check_started__isnull=True))
-                # ASAP check NGOs which were scheduled in the past but the check did not finish:
-                | Q(Q(cult_registry_check_ended__isnull=True) & Q(cult_registry_check_started__lte=recent_deadline))
-                # After a longer deadline check NGOs which were already present in the registry:
-                | Q(Q(is_in_cult_registry=True) & Q(cult_registry_check_started__lte=long_deadline))
-                # After a shorter deadline check NGOs which are were still absent from the registry:
-                | Q(Q(is_in_cult_registry=False) & Q(cult_registry_check_started__lte=short_deadline))
-            )
-            .order_by("-date_created")
-            .values_list("registration_number", flat=True)[: settings.ANAF_CULT_REGISTRY_BATCH_SIZE]
+            # check NGOs which are were not yet checked in the registry:
+            .filter(Q(is_in_cult_registry__isnull=True, cult_registry_check_started__isnull=True))
+            .distinct("registration_number")
+            .values_list("registration_number", flat=True)
         )
+        registration_numbers: set[str] = set(qs[: settings.ANAF_CULT_REGISTRY_BATCH_SIZE])
 
         result = cult_registry_check_organizations(list(registration_numbers), "sync")
-        print(result)
+
+        self.stdout.write(
+            f"Checked {len(registration_numbers)} NGOs in ANAF Cult Registry: "
+            f"{result.get('present')} present, {result.get('absent')} absent, {result.get('error')} error"
+        )
