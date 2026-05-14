02f9e46d7c9f2da2424a17891219da71d49c0283
Daniel Ursache <daniel.ursache@commitglobal.org>
2026-05-14 11:05:34 +0300

    Handle missing request headers for partner subdomains

--- backend/partners/context_processors.py
+++ backend/partners/context_processors.py
@@ -2,6 +2,11 @@ from django.http import HttpRequest
 
 
 def custom_subdomain(request: HttpRequest):
+    try:
+        custom_subdomain = request.partner.subdomain if request.partner else ""
+    except AttributeError:
+        custom_subdomain = ""
+
     return {
-        "custom_subdomain": request.partner.subdomain if request.partner else "",
+        "custom_subdomain": custom_subdomain,
     }
