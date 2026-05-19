f9641f906f3a5a0b597e73df7e392f242daf29b9
Daniel Ursache <daniel.ursache@commitglobal.org>
2026-05-19 15:55:45 +0300

    Handle missing request attributes for user context processor

--- backend/users/context_processors.py
+++ backend/users/context_processors.py
@@ -3,10 +3,14 @@ from django.http import HttpRequest
 
 
 def get_admin_properties(request: HttpRequest):
-    user = request.user
+    try:
+        user = request.user
+    except AttributeError:
+        user = None
+
     return {
-        "is_admin": user.has_perm("users.can_view_old_dashboard"),
-        "is_staff": user.is_staff,
+        "is_admin": user.has_perm("users.can_view_old_dashboard") if user else False,
+        "is_staff": user.is_staff if user else False,
         "SOCIALACCOUNT_ENABLED": settings.SOCIALACCOUNT_ENABLED,
         "SOCIALACCOUNT_ONLY": settings.SOCIALACCOUNT_ONLY,
     }
